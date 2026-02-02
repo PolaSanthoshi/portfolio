1b6235fd4994896b802a1a54826e676a5f847f19
PolaSanthoshi <polasanthoshi39@gmail.com>
2026-02-02 14:33:00 +0530

    Ready for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 const path = require('path');
 
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = '/portfolio'; // your GitHub repo name
-
 module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -10,10 +7,12 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  output: 'export', // ✅ required for static export
+
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -27,7 +26,4 @@ module.exports = {
       },
     ],
   },
-  // Added for GitHub Pages
-  basePath: isProd ? repoName : '',
-  assetPrefix: isProd ? repoName : '',
 };
